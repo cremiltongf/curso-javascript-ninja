61a9402e1232c99d440aa9a73ae7337ed03ec7d6
Cremilton Fernandes <cremilton@outlook.com>
2024-07-24 23:44:15 -0300

    Resolução do desafio 29

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -1,4 +1,4 @@
-(function() {
+(function( DOM ) {
   'use strict';
 
   /*
@@ -36,4 +36,72 @@
   que será nomeado de "app".
   */
 
-})();
+  function app() {
+    return {
+      init: function init() {
+        console.log( 'app init' );
+        this.companyInfo();
+        this.initEvents();
+      },
+
+      initEvents: function initEvents() {
+        let $submit = DOM( '[data-js="formulario-de-cadastro"]' );
+        $submit.on( 'submit', this.handleSubmit );
+      },
+
+      handleSubmit: function handleSubmit( e ) {
+        e.preventDefault();
+        let $dadosInput = DOM( '[data-js="dados-input"]' ).get();
+        $dadosInput.appendChild( app().createNewCar() );
+      },
+
+      createNewCar: function createNewCar() {
+        let $fragment = document.createDocumentFragment();
+        let $tr = document.createElement( 'tr' );
+        let $imagem = document.createElement( 'img' );
+        let $tdImagem = document.createElement( 'td' );
+        let $tdModelo = document.createElement( 'td' );
+        let $tdAno = document.createElement( 'td' );
+        let $tdPlaca = document.createElement( 'td' );
+        let $tdCor = document.createElement( 'td' );
+        $imagem.src = DOM( '[data-js="imagem"]' ).get().value;
+        $tdImagem.appendChild( $imagem );
+        $tdModelo.textContent = DOM( '[data-js="modelo"]' ).get().value;
+        $tdAno.textContent = DOM( '[data-js="ano"]' ).get().value;
+        $tdPlaca.textContent = DOM( '[data-js="placa"]' ).get().value;
+        $tdCor.textContent = DOM( '[data-js="cor"]' ).get().value;
+        $tr.appendChild($tdImagem);
+        $tr.appendChild($tdModelo);
+        $tr.appendChild($tdAno);
+        $tr.appendChild($tdPlaca);
+        $tr.appendChild($tdCor);
+        return $fragment.appendChild($tr);
+      },
+
+      companyInfo: function companyInfo() {
+        let ajax = new XMLHttpRequest();
+        ajax.open( 'GET', '/challenge-29/company.json', true );
+        ajax.send();
+        ajax.addEventListener( 'readystatechange', this.getCompanyInfo, false );
+      },
+
+      getCompanyInfo: function getCompanyInfo() {
+        if( !app().isReady.call( this ) )
+          return;
+
+        let data = JSON.parse( this.responseText );
+        let $nomeEmpresa = DOM( '[data-js="nome-empresa"]' ).get();
+        let $foneEmpresa = DOM( '[data-js="telefone-empresa"]' ).get();
+        $nomeEmpresa.textContent = data.name;
+        $foneEmpresa.textContent = data.phone;
+      },
+
+      isReady: function isReady() {
+        return this.readyState === 4 && this.status === 200;
+      }
+    };
+  }
+
+  app().init();
+
+})( window.DOM );
